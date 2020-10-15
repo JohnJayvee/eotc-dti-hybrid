49eaab3a1ef075add3a01f668d4604bdc6be750d
ronniecastro <ronnie.castro54@gmail.com>
2020-10-15 16:22:16 +0800

    update

--- app/Laravel/Controllers/System/TransactionController.php
+++ app/Laravel/Controllers/System/TransactionController.php
@@ -470,7 +470,7 @@ class TransactionController extends Controller{
 			}
 			if ($type == "DECLINED") {
 				$requirements = TransactionRequirements::where('transaction_id',$transaction->id)->where('status',"pending")->update(['status' => "DECLINED"]);
-				/*$insert[] = [
+				$insert[] = [
 	            	'contact_number' => $transaction->contact_number,
 	                'ref_num' => $transaction->document_reference_code,
 	                'email' => $transaction->email,
@@ -486,7 +486,7 @@ class TransactionController extends Controller{
 			    Event::dispatch('send-sms-declined', $notification_data);
 
 			    $notification_data_email = new SendDeclinedEmailReference($insert);
-			    Event::dispatch('send-email-declined', $notification_data_email);*/
+			    Event::dispatch('send-email-declined', $notification_data_email);
 			}
 			
 
