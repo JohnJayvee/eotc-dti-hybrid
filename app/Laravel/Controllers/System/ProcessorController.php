de64fe85d1668968f73c4cd1adf8803a48e3a18d
ronniecastro <ronnie.castro54@gmail.com>
2020-11-25 11:02:37 +0800

    update

--- app/Laravel/Controllers/System/ProcessorController.php
+++ app/Laravel/Controllers/System/ProcessorController.php
@@ -148,12 +148,12 @@ class ProcessorController extends Controller
 					'ref_id' => $new_processor->reference_id,
 	                'contact_number' => $new_processor->contact_number,
 	                'otp' => $new_processor->otp,
-	                'type' => $new_processor->type
+	                'type' => $new_processor->type,
 	                'email' => $new_processor->email
 	            ];	
 				$notification_data = new SendProcessorReference($insert);
 			    Event::dispatch('send-sms-processor', $notification_data);
-			    
+
 			    $notification_email_data = new SendEmailProcessorReference($insert);
 		    	Event::dispatch('send-email-reference', $notification_email_data);
 
